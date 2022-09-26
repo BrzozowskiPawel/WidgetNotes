5cf7228e68f7d841f0b0e0f3fbdb41fe5cff18fc
BrzozowskiPawel <pawelbrzozowskigplus@gmail.com>
2022-09-26 20:46:53 +0200

    Initial commit

---
+++ Days/Days/DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysApp.swift
+//  Days
+//
+//  Created by Paweł Brzozowski on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
