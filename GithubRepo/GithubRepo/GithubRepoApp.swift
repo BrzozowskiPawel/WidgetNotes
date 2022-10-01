a651f78f872fd042482d7a7750a1a0bf76b3a74e
BrzozowskiPawel <pawelbrzozowskigplus@gmail.com>
2022-10-01 18:32:25 +0200

    Created a project

---
+++ GithubRepo/GithubRepo/GithubRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepoApp.swift
+//  GithubRepo
+//
+//  Created by Paweł Brzozowski on 01/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct GithubRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
